cfd5140a4e1b95894704123c2af3f83e3b772cd8
Carson Krueger <crkruege@gmail.com>
2024-09-11 15:10:54 -0600

    init back-end commit

---
+++ back-end/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+// import { AppConfiguration } from "@angular-environment-variables/app-configuration";
+// const express = require("express");
+
+const port = 3000;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});

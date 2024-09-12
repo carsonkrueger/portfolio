76cdbd5bc270f9e6a628df5ae39e5d595f8bef70
Carson Krueger <crkruege@gmail.com>
2024-09-11 20:06:53 -0600

    fix serving of client

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 // import { AppConfiguration } from "@angular-environment-variables/app-configuration";
 // const express = require("express");
 
@@ -8,6 +9,14 @@ dotenv.config();
 const port = process.env.PORT ?? 3000;
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/../../front-end/dist/browser")));
+
+app.use("*", (_req, res) => {
+  res.sendFile(
+    path.join(__dirname, "/../../front-end/dist/browser/index.html"),
+  );
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);

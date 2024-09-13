bf2ce22f0a76886492457630f2646ec02e7da7c4
Carson Krueger <crkruege@gmail.com>
2024-09-13 10:44:49 -0600

    change home bg and colors

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
       spacing: {
         "nav-width": "var(--nav-width)",
       },
+      backgroundImage: {
+        peaks: "url('/peak_background.svg')",
+      },
+      colors: {
+        primary: "#F95164",
+        secondary: "#FFCC00",
+      },
     },
   },
   plugins: [],

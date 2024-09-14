30e40b358e39e151885f21c6a5989b04221bce37
Carson Krueger <crkruege@gmail.com>
2024-09-14 15:49:59 -0600

    add download resume to client

--- front-end/src/app/about/about.component.ts
+++ front-end/src/app/about/about.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-about',
+  selector: "app-about",
   standalone: true,
   imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  templateUrl: "./about.component.html",
+  styleUrl: "./about.component.css",
 })
 export class AboutComponent {
+  blob: Blob | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  downloadResume() {
+    if (this.blob === null) {
+      this.http.get("/resume", { responseType: "blob" }).subscribe((res) => {
+        this.blob = new Blob([res as any], { type: "application/pdf" });
+      });
+    }
+
+    if (this.blob !== null) {
+      const url = window.URL.createObjectURL(this.blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "resume-carson-krueger.pdf";
+      link.click();
+    }
+  }
 }

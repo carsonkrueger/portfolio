76ff5f83566b2ed15e448b3a572ddc5f3d8bdb35
Carson Krueger <crkruege@gmail.com>
2024-09-17 22:50:34 -0600

    fix resume download on first click

--- front-end/src/app/about/about.component.ts
+++ front-end/src/app/about/about.component.ts
@@ -17,15 +17,19 @@ export class AboutComponent {
     if (this.blob === null) {
       this.http.get("/resume", { responseType: "blob" }).subscribe((res) => {
         this.blob = new Blob([res as any], { type: "application/pdf" });
+        this.clickDownload();
       });
+    } else if (this.blob !== null) {
+      this.clickDownload();
     }
+  }
 
-    if (this.blob !== null) {
-      const url = window.URL.createObjectURL(this.blob);
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = "resume-carson-krueger.pdf";
-      link.click();
-    }
+  clickDownload() {
+    if (this.blob === null) return;
+    const url = window.URL.createObjectURL(this.blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "resume-carson-krueger.pdf";
+    link.click();
   }
 }

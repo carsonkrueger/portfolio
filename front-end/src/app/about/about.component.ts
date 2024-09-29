953ff61b25ce0ad864f366fa0d05bb86d44794e6
Carson Krueger <crkruege@gmail.com>
2024-09-29 16:42:12 -0600

    move download resume fn to nav service

--- front-end/src/app/about/about.component.ts
+++ front-end/src/app/about/about.component.ts
@@ -16,40 +16,11 @@ import { NavService } from "../../services/nav.service";
     styleUrl: "./about.component.css",
 })
 export class AboutComponent implements AfterViewInit {
-    blob: Blob | null = null;
     @ViewChild("aboutRef") aboutRef!: ElementRef;
 
-    constructor(
-        private http: HttpClient,
-        private navService: NavService,
-    ) {}
-
-    downloadResume() {
-        if (this.blob === null) {
-            this.http
-                .get("/resume", { responseType: "blob" })
-                .subscribe((res) => {
-                    this.blob = new Blob([res as any], {
-                        type: "application/pdf",
-                    });
-                    this.clickDownload();
-                });
-        } else if (this.blob !== null) {
-            this.clickDownload();
-        }
-    }
-
-    clickDownload() {
-        if (this.blob === null) return;
-        const url = window.URL.createObjectURL(this.blob);
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = "resume-carson-krueger.pdf";
-        link.click();
-    }
+    constructor(protected navService: NavService) {}
 
     ngAfterViewInit(): void {
-        console.log(this.aboutRef);
         this.navService.aboutRef = this.aboutRef;
     }
 
--- front-end/src/services/nav.service.ts
+++ front-end/src/services/nav.service.ts
@@ -1,18 +1,45 @@
+import { HttpClient } from "@angular/common/http";
 import { ElementRef, Injectable } from "@angular/core";
 
 @Injectable({
     providedIn: "root",
 })
 export class NavService {
+    private blob?: Blob;
+
     public aboutRef?: ElementRef;
     public skillsRef?: ElementRef;
     public projectsRef?: ElementRef;
     public experiencesRef?: ElementRef;
     public contactRef?: ElementRef;
 
-    constructor() {}
+    constructor(private http: HttpClient) {}
 
     public onRefClick(ref?: ElementRef) {
         ref?.nativeElement.scrollIntoView({ behavior: "smooth" });
+    }
+
+    downloadResume() {
+        if (this.blob === undefined) {
+            this.http
+                .get("/resume", { responseType: "blob" })
+                .subscribe((res) => {
+                    this.blob = new Blob([res as any], {
+                        type: "application/pdf",
+                    });
+                    this.clickDownload();
+                });
+        } else if (this.blob !== undefined) {
+            this.clickDownload();
+        }
+    }
+
+    clickDownload() {
+        if (this.blob === undefined) return;
+        const url = window.URL.createObjectURL(this.blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = "resume-carson-krueger.pdf";
+        link.click();
     }
 }

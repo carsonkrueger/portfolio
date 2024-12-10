2a4d89d56a798523bacad69519c6fa810057c448
Carson Krueger <crkruege@gmail.com>
2024-12-09 20:55:09 -0700

    fix cors and resume download

--- front-end/src/services/nav.service.ts
+++ front-end/src/services/nav.service.ts
@@ -1,45 +1,46 @@
 import { HttpClient } from "@angular/common/http";
 import { ElementRef, Injectable } from "@angular/core";
+import { BASE_URL } from "../util/http-client";
 
 @Injectable({
-    providedIn: "root",
+  providedIn: "root",
 })
 export class NavService {
-    private blob?: Blob;
+  private blob?: Blob;
 
-    public aboutRef?: ElementRef;
-    public skillsRef?: ElementRef;
-    public projectsRef?: ElementRef;
-    public experiencesRef?: ElementRef;
-    public contactRef?: ElementRef;
+  public aboutRef?: ElementRef;
+  public skillsRef?: ElementRef;
+  public projectsRef?: ElementRef;
+  public experiencesRef?: ElementRef;
+  public contactRef?: ElementRef;
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-    public onRefClick(ref?: ElementRef) {
-        ref?.nativeElement.scrollIntoView({ behavior: "smooth" });
+  public onRefClick(ref?: ElementRef) {
+    ref?.nativeElement.scrollIntoView({ behavior: "smooth" });
+  }
+
+  public downloadResume = () => {
+    if (this.blob === undefined) {
+      this.http
+        .get(`${BASE_URL}/resources/Resume.pdf`, { responseType: "blob" })
+        .subscribe((res) => {
+          this.blob = new Blob([res as any], {
+            type: "application/pdf",
+          });
+          this.clickDownload();
+        });
+    } else if (this.blob !== undefined) {
+      this.clickDownload();
     }
+  };
 
-    public downloadResume = () => {
-        if (this.blob === undefined) {
-            this.http
-                .get("/resources/Resume.pdf", { responseType: "blob" })
-                .subscribe((res) => {
-                    this.blob = new Blob([res as any], {
-                        type: "application/pdf",
-                    });
-                    this.clickDownload();
-                });
-        } else if (this.blob !== undefined) {
-            this.clickDownload();
-        }
-    };
-
-    private clickDownload = () => {
-        if (this.blob === undefined) return;
-        const url = window.URL.createObjectURL(this.blob);
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = "resume-carson-krueger.pdf";
-        link.click();
-    };
+  private clickDownload = () => {
+    if (this.blob === undefined) return;
+    const url = window.URL.createObjectURL(this.blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "resume-carson-krueger.pdf";
+    link.click();
+  };
 }
